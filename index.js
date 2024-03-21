98fdb3e0f271f564b6591f60b9bcf75351743d9b
Samson Mubeya <rasspartah@gmail.com>
2024-03-22 00:24:51 +0300

    did my assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Code your solution in this file!
+const companyName="Scuber";
+let mostProfitableNeighborhood="Chelsea";
+let companyCeo="Susan Smith";
+
+console.log("A Companny by the name "+companyName+" is just as profitable as "+mostProfitableNeighborhood+" lead by the company CEO Ms "+companyCeo);
